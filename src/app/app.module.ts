4eec00eb7fd69b8c78cffeddd42caa7beb97ca8e
Alan Cortes <ing.ti.upmh@gmail.com>
2021-08-28 02:02:08 -0500

    Rutas principales de la aplicación
    Se implementa modulo encragado de rutas principales de la aplicación.
    
    Se realizan ajustes generales en archivos HTML del proyecto.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
@@ -24,7 +26,8 @@ import { FooterComponent } from './shared/footer/footer.component';
     FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
