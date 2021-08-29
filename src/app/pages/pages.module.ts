aa0baa7fa4251a29c667b4150b6822d58c36fd8d
Alan Cortes <ing.ti.upmh@gmail.com>
2021-08-28 19:05:53 -0500

    Creaciòn de modulos
    - Módulo pages, shared y auth para simplificar app.module.ts

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PagesComponent } from './pages.component';
+import { SharedModule } from '../shared/shared.module';
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    PagesComponent
+  ],
+  exports: [
+    DashboardComponent,
+    PagesComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    RouterModule
+  ]
+})
+export class PagesModule { }
