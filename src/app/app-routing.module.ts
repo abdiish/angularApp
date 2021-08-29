48f9023fe44d403b10827a9dfa87fcb059442a7a
Alan Cortes <ing.ti.upmh@gmail.com>
2021-08-28 19:28:20 -0500

    Rutas hijas - ForChild
    Se crea modulo para rutas hijas, esto con la finalidad de no saturar el
    sistema de rutas principales.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
+// Modulos
+import { PagesRoutingModule } from './pages/pages.routing';
+
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
+
 
 
 const routes: Routes = [
-  { 
-    path: '', component: PagesComponent,
-    children: [
-        // Rutas protegidas/rutas hijas 
-        { path: 'dashboard', component: DashboardComponent },
-        { path: '', redirectTo:'/dashboard', pathMatch:'full' }, // path vacio, redirecciona a dashboard
-    ]
-  },
- 
   // Rutas publicas
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
@@ -26,7 +20,10 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [],
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    PagesRoutingModule
+  ],
   exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
---
+++ src/app/pages/pages.routing.ts
@@ -0,0 +1,22 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const routes: Routes = [
+
+    { 
+        path: '', component: PagesComponent,
+        children: [
+            // Rutas protegidas/rutas hijas 
+            { path: 'dashboard', component: DashboardComponent },
+            { path: '', redirectTo:'/dashboard', pathMatch:'full' }, // path vacio, redirecciona a dashboard
+        ]
+      },
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class PagesRoutingModule {}
