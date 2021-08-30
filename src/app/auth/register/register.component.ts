32cb309dbf0aae97ca7aae83d0d37e45e5bb8527
Alan Cortes <ing.ti.upmh@gmail.com>
2021-08-30 15:29:10 -0500

    Uso de RouterLink
    -Direccionar Logout a pantalla de Login
    -Direccoionar Login a pantalla de Dashboard

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -8,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
+
+  login() {
+    this.router.navigateByUrl('/');
+  }
+
 
 }
