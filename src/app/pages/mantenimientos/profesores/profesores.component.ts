16fcd2590b9362e50d0e3cb89f0af221a329bbea
Alan Cortes <ing.ti.upmh@gmail.com>
2021-08-31 10:47:34 -0500

    Mantenimientos
    Creación de componente Mantenimientos y maquetado HTML:
    
    -Componente alumnos
    -Componente profesores
    -Componente usuarios

---
+++ src/app/pages/mantenimientos/profesores/profesores.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profesores',
+  templateUrl: './profesores.component.html',
+  styles: [
+  ]
+})
+export class ProfesoresComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/pages/mantenimientos/usuarios/usuarios.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-usuarios',
+  templateUrl: './usuarios.component.html',
+  styles: [
+  ]
+})
+export class UsuariosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  abrirModal() {
+    
+  }
+}
