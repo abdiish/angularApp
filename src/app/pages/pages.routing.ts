e76c2dfb7943e8da9edd3b17d99b2b4a85cbafdb
Alan Cortes <ing.ti.upmh@gmail.com>
2021-08-31 10:55:59 -0500

    Rutas Hijas
    Implementación de rutas para Mantenimientos.

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -4,15 +4,25 @@ import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AsistenciaComponent } from './asistencia/asistencia.component';
 
+//Mantenimientos
+import { UsuariosComponent } from './mantenimientos/usuarios/usuarios.component';
+import { AlumnosComponent } from './mantenimientos/alumnos/alumnos.component';
+import { ProfesoresComponent } from './mantenimientos/profesores/profesores.component';
+
 const routes: Routes = [
 
     { 
         path: 'dashboard', component: PagesComponent,
         children: [
             // Rutas protegidas/rutas hijas 
-            { path: '', component: DashboardComponent },
-            { path: 'asistencia', component: AsistenciaComponent },
+            { path: '', component: DashboardComponent, data: { titulo: 'Dashboard'} },
+            { path: 'asistencia', component: AsistenciaComponent, data: { titulo: 'Asistencia de Alumnos'} },
 
+            //Mantenimientos
+            { path: 'usuarios', component: UsuariosComponent, data: { titulo: 'Usuarios de la aplicación'} },
+            { path: 'alumnos', component: AlumnosComponent, data: { titulo: 'Alumnos'} },
+            { path: 'profesores', component: ProfesoresComponent, data: { titulo: 'Profesores'} },
+            { path: 'modal-imagen', component: ProfesoresComponent, data: { titulo: 'Modal'} },
         ]
       },
 ];
