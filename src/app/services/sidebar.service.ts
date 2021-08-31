21195cc50efe4a0c3eb0eecec3abdcea11e39c32
Alan Cortes <ing.ti.upmh@gmail.com>
2021-08-31 10:53:06 -0500

    Servicio- Sidebar
    Implementación de menú mantenimientos, sidebar de la aplicación.

--- src/app/services/sidebar.service.ts
+++ src/app/services/sidebar.service.ts
@@ -8,10 +8,20 @@ export class SidebarService {
   menu: any[] = [
     {
       titulo: 'Dashboard',
-      icono: 'nav-icon fas fa-tachometer-alt',
+      icono: 'nav-icon fas fa-school',
       submenu: [
-        { titulo: 'Principal', icono:'fas fa-school', url:'/' },
+        { titulo: 'Principal', url:'/' },
         { titulo: 'Asistencia', url:'asistencia' },
+      ]
+    },
+    {
+      titulo: 'Mantenimientos',
+      icono: 'nav-icon fas fa-cogs',
+      submenu: [
+        { titulo: 'Usuarios', url:'usuarios' },
+        { titulo: 'Alumnos', url:'alumnos' },
+        { titulo: 'Profesores', url:'profesores' },
+        //{ titulo: 'Asignaturas-Grupos', url:'asignaturas' },
       ]
     }
   ];
