f383217c80f04f2b277f1bcadb15ca35460ed271
Alan Cortes <ing.ti.upmh@gmail.com>
2021-09-24 13:03:58 -0500

    Small Box
    - Usuarios, Alumnos y Profesores registrados en la aplicación

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from '../../services/usuario.service';
+import { AlumnosService } from '../../services/alumno.service';
+import { ProfesorService } from '../../services/profesor.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,9 +11,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  public totalUsuarios  : number = 0;
+  public totalAlumnos   : number = 0;
+  public totalProfesores: number = 0;
+
+  constructor(private usuarioService: UsuarioService,
+              private alumnosService: AlumnosService,
+              private profesorService: ProfesorService) { }
 
   ngOnInit(): void {
+
+    this.cargarUsuarios();
+    this.cargarAlumnos();
+    this.cargarProfesores();
+  }
+
+  cargarUsuarios() {
+    this.usuarioService.cargarUsuarios( )
+      .subscribe( ({ total }) => {
+        this.totalUsuarios = total;
+      })
+  }
+
+  cargarAlumnos() {
+
+    this.alumnosService.cargarAlumnos()
+      .subscribe(({ total }) => {
+          this.totalAlumnos = total;
+      })
+  }
+
+  cargarProfesores() {
+
+    this.profesorService.cargarProfesores()
+      .subscribe(({ total }) => {
+        this.totalProfesores = total;
+      })
   }
 
 }
